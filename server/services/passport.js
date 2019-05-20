dfcc8c80e72a9664c7c43e0ada748eb5272b281a
mazdak <nazemi.works@gmail.com>
2019-05-20 06:16:24 -0700

    check add existance users from db in callback of passport also use serialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users'); //one arg means fetch 2 means import sth in
+
+//this is that existence user
+passport.serializeUser((user, done)=>{
+  done(null, user.id); //its not profile id.
+})
+
+
 //inform passport to use g strategy
 passport.use(new GoogleStrategy({ // i am 'google'
   clientID: keys.googleClientID, //need client id & secret
@@ -11,7 +18,13 @@ passport.use(new GoogleStrategy({ // i am 'google'
   callbackURL: '/auth/google/callback' //=>where we send user back after access permission
 }, //that profile.id we want in our db
 (accessToken, refreshToken, profile, done) => { 
-  
-  new User({ googleId: profile.id }).save();
+  User.findOne({ googleId: profile.id })
+    .then( existUser =>{
+      if (existUser) done(null, existUser)
+      else {
+        new User({ googleId: profile.id }).save()
+          .then(user => done(null,user));  
+      };
+    });
 }));
 //after step from authRoutes strategy do this give access token to guys
