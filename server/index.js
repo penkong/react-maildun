75df1a8afa6cd2c37c40399e7c91d4a28493ce21
mazdak <nazemi.works@gmail.com>
2019-05-20 01:17:39 -0700

    add passport and g strategy for ss oauth

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //we want only this prop
 
 const app = express();
-
+//inform passport to use g strategy
+passport.use(new GoogleStrategy()); //need client id & secret
 
 app.get('/',(req,res)=>{
   res.send({hi:'there and oheran'});
