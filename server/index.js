f1dcb50d54fd9f83546aa48a0e9b2e0fada41942
mazdak <nazemi.works@gmail.com>
2019-05-20 05:06:32 -0700

    connect mongo to app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 require('./services/passport'); //logic and helper for auth in google
 
-
+//>>-----------DB connection -------------------------
+mongoose.connect(keys.mongoURI , { useNewUrlParser: true });
 
 const app = express();
 require('./routes/authRoutes')(app); //route for google 
