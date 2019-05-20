6e09aa1eeb4565a349be1e665d39f6f55675409d
mazdak <nazemi.works@gmail.com>
2019-05-20 05:38:05 -0700

    connect and create first user in mongodb with mongoose  through express

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys')
-require('./services/passport'); //logic and helper for auth in google
 
+// ------- first stage load up guys
+require('./models/User');
+require('./services/passport'); //logic and helper for auth in google
 //>>-----------DB connection -------------------------
 mongoose.connect(keys.mongoURI , { useNewUrlParser: true });
 
 const app = express();
-require('./routes/authRoutes')(app); //route for google 
+require('./routes/authRoutes')(app); //route for google and exchange code when user back from auth
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log('listening on'));
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; //we want only this prop
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-
+const User = mongoose.model('users'); //one arg means fetch 2 means import sth in
 //inform passport to use g strategy
 passport.use(new GoogleStrategy({ // i am 'google'
   clientID: keys.googleClientID, //need client id & secret
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback' //=>where we send user back after access permission
-}, (accessToken, refreshToken, profile, done) => { //after step below strategy do this
-  console.log('1' + accessToken);
-  console.log('2' + refreshToken);
-  console.log('3' + profile);
+}, //that profile.id we want in our db
+(accessToken, refreshToken, profile, done) => { 
+  
+  new User({ googleId: profile.id }).save();
 }));
+//after step from authRoutes strategy do this give access token to guys
