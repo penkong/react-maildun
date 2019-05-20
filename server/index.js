78aff3e3e19a36dee34fac5147e08a5f69921b71
mazdak <nazemi.works@gmail.com>
2019-05-20 01:46:44 -0700

    add config file for set passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy; //we want only this prop
+const keys = require('./config/keys');
+
 
 const app = express();
 //inform passport to use g strategy
-passport.use(new GoogleStrategy()); //need client id & secret
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback' //=>where we send user back after access permission
+  }, accessToken => {
+    console.log(accessToken);
+  })
+); //need client id & secret
 
 app.get('/',(req,res)=>{
   res.send({hi:'there and oheran'});
