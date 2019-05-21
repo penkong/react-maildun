8262054edf9fc7873bd105b7a656167daa970098
mazdak <nazemi.works@gmail.com>
2019-05-21 02:26:14 -0700

    reducers add and redux to proj client side

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+//reducers , initial , middleware
+const store = createStore(reducers, {}, applyMiddleware());
 
-
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.querySelector('#root')
+);
