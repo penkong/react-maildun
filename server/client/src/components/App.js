3105f16dd90f9741fcc48fd58f5fe84849f2dc71
mazdak <nazemi.works@gmail.com>
2019-05-21 06:37:11 -0700

    flow of on auth action

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header/>            
-          {/* <Route path="/" exact component={Landing}/>
-          <Route path="/surveys" exact component={Dashboard}/>
-          <Route path="/surveys/new" exact component={SurveyNew}/>
-          <Route path="/" exact component={Landing}/> */}
-        </div>
-      </BrowserRouter>
-    </div>
-  )
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header/>            
+            {/* <Route path="/" exact component={Landing}/>
+            <Route path="/surveys" exact component={Dashboard}/>
+            <Route path="/surveys/new" exact component={SurveyNew}/>
+            <Route path="/" exact component={Landing}/> */}
+          </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+// // const mapStateToProps = state => {
+//   return { auth: state.auth };
+// }
+
+
+export default connect(null, {fetchUser})(App);
+
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,8 +1,10 @@
+import { FETCH_USER } from '../actions/types';
 
-
-export default (state = {}, action) => {
+export default (state = null, action) => {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false ;
     default: 
       return state;
   }
-}
+} 
