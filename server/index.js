9e282d4bcc25672f614884d828f74613424a07ac
mazdak <nazemi.works@gmail.com>
2019-05-31 08:50:10 -0700

    for heroku

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session'); //to inform express for cookie manager duo to serialize them
-const passport = require('passport'); //we tell password keep track of sessions with cookie package
-const keys = require('./config/keys')
+// const mongoose = require('mongoose');
+// const cookieSession = require('cookie-session'); //to inform express for cookie manager duo to serialize them
+// const passport = require('passport'); //we tell password keep track of sessions with cookie package
+// const keys = require('./config/keys')
 
-// ------- first stage load up guys ------------------
-require('./models/User');
-require('./services/passport'); //logic and helper for auth in google
-//>>-----------DB connection -------------------------
-mongoose.connect(keys.mongoURI , { useNewUrlParser: true });
-//>>---------- Route Games ---------------------------
+// // ------- first stage load up guys ------------------
+// require('./models/User');
+// require('./services/passport'); //logic and helper for auth in google
+// //>>-----------DB connection -------------------------
+// mongoose.connect(keys.mongoURI , { useNewUrlParser: true });
+// //>>---------- Route Games ---------------------------
 const app = express();
-//.............for encryption cookie .................
-app.use(cookieSession({ //config obj
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey] //use cookie key for encryption
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-//..... route
-require('./routes/authRoutes')(app); //route for google and exchange code when user back from auth
+app.get('/',(req,res)=>{
+  res.send({hi:'there'});
+})
+// //.............for encryption cookie .................
+// app.use(cookieSession({ //config obj
+//   maxAge: 30 * 24 * 60 * 60 * 1000,
+//   keys: [keys.cookieKey] //use cookie key for encryption
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// //..... route
+// require('./routes/authRoutes')(app); //route for google and exchange code when user back from auth
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log('listening on'));
