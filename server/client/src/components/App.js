524df26f97df270a60478c636609b8df9404329a
mazdak <nazemi.works@gmail.com>
2019-06-03 21:24:49 -0700

    add boiler comps route and materialize css

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
-      dd
+      <BrowserRouter>
+        <div>
+          <Header/>
+          {/* <Route path="/" exact component={Landing}/>
+          <Route path="/surveys" exact component={Dashboard}/>
+          <Route path="/surveys/new" exact component={SurveyNew}/>
+          <Route path="/sd" exact component={More}/> */}
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,12 +1,15 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 
+import reducers from './reducers';
+
 import App from './components/App';
 
-const store = createStore(()=>[], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
