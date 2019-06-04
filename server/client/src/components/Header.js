7ad450ef2b02a9232979cf6aef3c41f820d81306
mazdak <nazemi.works@gmail.com>
2019-06-04 07:05:43 -0700

    add header state from redux store after fetchUser

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,19 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent(){
+    switch (this.props.auth) {
+      case null:
+        return; 
+      case false:
+        return (
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return (
+          <li><a href="#">working</a></li>
+        );
+    }
+  }  
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">FUll Email</a>
           <ul className="right">
-            <li><a>Login with google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     );
   }
 }
+const mapStateToProps = ({ auth }) => { return { auth }};
 
-export default Header;
+export default connect(mapStateToProps)(Header);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,8 +1,10 @@
-
-
-export default function (state = {}, action){
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+//null we dont know user is on or not
+export default function (state = null, action){
+  // console.log(action);
   switch (action.type){
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
