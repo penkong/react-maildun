7275e6b4c8d43c74a5493b323cd154e542c0e223
mazdak <nazemi.works@gmail.com>
2019-06-03 20:44:24 -0700

    add starter for redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 
 import App from './components/App';
 
+const store = createStore(()=>[], {}, applyMiddleware());
 
-ReactDOM.render(<App/> , document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+  <App/> 
+  </Provider>
+  , 
+  document.querySelector('#root')
+);
