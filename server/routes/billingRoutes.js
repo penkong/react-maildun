c98dcf5aca239660d58c0948f0cdd01a7fa5dd92
mazdak <nazemi.works@gmail.com>
2019-06-06 21:10:28 -0700

    make charge with stripe reusable

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,9 +4,15 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 
-
+//token from client come ad req.body
 module.exports = app =>{
-  app.post('/api/stripe',(req,res)=>{
-
+  app.post('/api/stripe', async (req,res)=>{
+    //now we do real work on api create payment that user pay in client
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 dollar for email',
+      source: req.body.id //id of who pay to us
+    })
   });
 };
