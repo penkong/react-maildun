570319b2c90b5d79f3790657832810a6f85a76d2
mazdak <nazemi.works@gmail.com>
2019-06-06 21:39:44 -0700

    add middleware for login require to reuse

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,11 +2,11 @@
 //stripe npm for node for change token we receive with actual amount in app
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
-
+const requireLogin = require('../middlewares/requireLogin');
 
 //token from client come ad req.body
 module.exports = app =>{
-  app.post('/api/stripe', async (req,res)=>{
+  app.post('/api/stripe', requireLogin, async (req,res)=>{
     //now we do real work on api create payment that user pay in client
     const charge = await stripe.charges.create({
       amount: 500,
@@ -14,5 +14,11 @@ module.exports = app =>{
       description: '$5 dollar for email',
       source: req.body.id //id of who pay to us
     })
-  });
+    //now we must give the credit to user store ui => update user model
+    //we have req.user by passport to help us authenticate every moment
+    req.user.credits += 5;
+    const user = await req.user.save();
+    //now send to client
+    res.send(user);
+  }); 
 };
