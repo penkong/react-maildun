4b15cb12f70c1a5fb3f6be4be08f8177cc59f165
mazdak <nazemi.works@gmail.com>
2019-06-07 04:55:08 -0700

    adding new route new middleware and complete doc of model

---
+++ middlewares/requireCredits.js
@@ -0,0 +1,8 @@
+
+
+
+module.exports = (req, res, next) => {
+  if (req.user.credits < 1) return res.status(403)
+    .send({ error: 'please increase your credits!' });
+  next();
+}
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,20 @@
-
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
+const requireCredits = require('../middlewares/requireCredits');
+//remember cause of testing we use this import method.
+const Survey = mongoose.model('surveys');
 module.exports = app => {
-  app.post('/api/surveys', requireLogin , (req,res)=>{
+  //create survey and send out big email out
+  app.post('/api/surveys', requireLogin, requireCredits, (req,res)=>{
+    const { title, subject, body, recipients } = req.body;
+    const survey = new Survey({
+      title, 
+      body, 
+      subject, 
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id, //id by mongoose generated.
+      dateSent: Date.now()
+    });
 
-  })
-}
+  });
+};
