0cb158d632b327e9be0804968670489b89ab5e5d
mazdak <nazemi.works@gmail.com>
2019-06-08 20:58:25 -0700

    make surveyField reuse

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+//survey field render a single label and text input.
+import React from 'react'
+
+const SurveyField = () => {
+  return (
+    <div>
+      <input/>
+    </div>
+  )
+}
+
+export default SurveyField
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,28 @@
 //show user input to add info
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+// all html el of form come here with Field comp
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields(){
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField}/>
+      </div>
+    )
+  }
   render() {
     return (
       <div>
-        surveyForm
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
-
+//name cause reduxForm save data with that label
 export default reduxForm({ form: 'surveyForm' })(SurveyForm);
+//reduxForm => handleSubmit
