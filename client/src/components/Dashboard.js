90e627b6751dc54c9a8b03bfa51663c16884d4bd
mazdak <nazemi.works@gmail.com>
2019-06-08 20:05:58 -0700

    add dashboard client side css and ...

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
     </div>
   )
 }
