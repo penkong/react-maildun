a298053a9dad27ff2219e1598d68c8bb65aee938
mazdak <nazemi.works@gmail.com>
2019-06-08 23:25:47 -0700

    validation by meta.error from reduxform

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,12 +1,15 @@
 //survey field render a single label and text input.
 import React from 'react'
 
-const SurveyField = ({ input, label}) => {
-  // console.log(input);
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
+  // console.log(meta);
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{marginBottom: '5px'}} />
+      <div className="red-text" style={{marginBottom: '20px'}}>
+      {touched && error}
+      </div>
     </div>
   )
 }
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 //show user input to add info
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 // all html el of form come here with Field comp
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
@@ -23,12 +24,27 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat left white-text" type="submit">Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
+
+//values is whole form values >> validate pass by meta
+const validate = (values) => {
+  const errors = {};
+  _.each(FIELDS, ({ name })=>{
+    if(!values[name]) errors[name] = `You must provide ${name}`;
+  });
+  return errors;
+}
+
+
 //name cause reduxForm save data with that label
-export default reduxForm({ form: 'surveyForm' })(SurveyForm);
+export default reduxForm({ validate , form: 'surveyForm' })(SurveyForm);
 //reduxForm => handleSubmit
