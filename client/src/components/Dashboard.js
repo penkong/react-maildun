5e97f14b41556a7f1e7d3222cccb50dc59e3dee0
mazdak <nazemi.works@gmail.com>
2019-06-13 05:17:29 -0700

    added fontawsome for add in surveys

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SurveyList from './surveys/SurveyList';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons'
+// const styles = {
+//   border: "1px dotted white",
+//   width: "3px",
+//   height: "3px",
+//   color: "inherit",
+//   backgroundColor: "black",
+//   boxSizing: "border-box",
+//   transform: "scale(11)",
+// }
 const Dashboard = () => {
   return (
-    <div className="bg-near-white h-100 w-100 center" >
-      <div className="tc pa4 w-10 h-auto center">
-        <Link to="/surveys/new" className="br-100 pa2 h3 w5 bg-red hover-bg-green white link fw8 f4">
-          Send mass emails
-        </Link>
+    <div className="flex flex-column" >
+      <Link to="/surveys/new" className="w-30 tc center navy hover-green pa3 mv3 no-underline" style={{fontFamily: "Roboto"}}>
+        <FontAwesomeIcon  size="3x" icon={faMailBulk} />
+        <span className="f6 f4-ns b ml2">SEND EMAILS</span> 
+      </Link>
+      <div className="cf bg-near-white h-100 w-100 center" >
+        <SurveyList/>
       </div>
-      <SurveyList/>
     </div>
   )
 }
