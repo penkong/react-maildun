097fd431ebb539afd5dfb910e87cac285e12b70c
mazdak <nazemi.works@gmail.com>
2019-06-15 21:22:54 -0700

    dashboard done refactor styles

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,31 @@ import { Link } from 'react-router-dom';
 import SurveyList from './surveys/SurveyList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons'
-// const styles = {
+
+const styles = {
+  dashboardPage: 'flex flex-column',
+  newEmails: 'w-30 center tc navy hover-green no-underline mv3 pa3',
+  newEmailsText: 'f6 f4-ns b ml2',
+  list: 'cf h-100 w-100 center bg-near-white '
+}
+
+const Dashboard = () => {
+  return (
+    <div className={styles.dashboardPage} >
+      <Link to="/surveys/new" className={styles.newEmails} style={{fontFamily: "Roboto"}}>
+        <FontAwesomeIcon  size="3x" icon={faMailBulk} />
+        <span className={styles.newEmailsText}>SEND EMAILS</span> 
+      </Link>
+      <div className={styles.list} >
+        <SurveyList/>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard;
+
+// const stylesPlus = {
 //   border: "1px dotted white",
 //   width: "3px",
 //   height: "3px",
@@ -12,18 +36,3 @@ import { faMailBulk } from '@fortawesome/free-solid-svg-icons'
 //   boxSizing: "border-box",
 //   transform: "scale(11)",
 // }
-const Dashboard = () => {
-  return (
-    <div className="flex flex-column" >
-      <Link to="/surveys/new" className="w-30 tc center navy hover-green pa3 mv3 no-underline" style={{fontFamily: "Roboto"}}>
-        <FontAwesomeIcon  size="3x" icon={faMailBulk} />
-        <span className="f6 f4-ns b ml2">SEND EMAILS</span> 
-      </Link>
-      <div className="cf bg-near-white h-100 w-100 center" >
-        <SurveyList/>
-      </div>
-    </div>
-  )
-}
-
-export default Dashboard
