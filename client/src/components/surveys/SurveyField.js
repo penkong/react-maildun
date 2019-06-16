2e80cf58d88b91c89243f56dc22e5772223019d1
mazdak <nazemi.works@gmail.com>
2019-06-15 21:30:03 -0700

    refactor styles of surveyField

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,17 +1,26 @@
 //survey field render a single label and text input.
 import React from 'react'
 
+const styles = {
+  field: 'cf w-100 pa2',
+  label: 'fl w-100 w-25-ns navy mv2 ml2-ns',
+  labelText: 'f6 f4-l',
+  input: 'fl fr-m fr-l w-100 w-70-ns',
+  inputText: 'w-100 ba b--lightest-blue pv2 ph1',
+  error: 'fr w-100 tr dark-pink mt1 pa1 pl2'
+}
+
 const SurveyField = ({ input, label, meta: { error, touched } }) => {
   // console.log(meta);
   return (
-    <div className="pa2 cf w-100">
-      <div className="fl w-100 w-25-ns navy mv2 ml2-ns">
-        <label className="f6 f4-l">{label}</label>
+    <div className={styles.field}>
+      <div className={styles.label}>
+        <label className={styles.labelText}>{label}</label>
       </div>
-      <div className="fl fr-m fr-l w-100 w-70-ns">
-        <input className="ba b--lightest-blue pv2 ph1 w-100" {...input}/>
+      <div className={styles.input}>
+        <input className={styles.inputText} {...input}/>
       </div>
-      <div className="fr w-100 tr dark-pink b pa1 pl2 mt1">
+      <div className={styles.error}>
         {touched && error}
       </div>
     </div>
